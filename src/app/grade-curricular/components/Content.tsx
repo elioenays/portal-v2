a3d2f33fdb80868eb6e72c2b813a59db39a5d83a
Elioenay Silva <elionay407@gmail.com>
2024-12-15 10:08:53 -0300

    cria map de status

--- src/app/grade-curricular/components/Content.tsx
+++ src/app/grade-curricular/components/Content.tsx
@@ -2,6 +2,19 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { GreadeDetails } from './GreadeDetails'
 import { ClassStatus } from './ClassStatus'
 
+type ClassStatuses = {
+  status?: 'concluida' | 'pendente' | 'naoConcluida' | 'default'
+  code: number
+  name: string
+}
+
+const classStatuses: ClassStatuses[] = [
+  { code: 1, name: 'Concluída', status: 'concluida' },
+  { code: 2, name: 'Pendente', status: 'pendente' },
+  { code: 3, name: 'Não Concluída', status: 'naoConcluida' },
+  { code: 4, name: 'Equivalente', status: 'default' },
+]
+
 export function GradeCurricularContent() {
   return (
     <Card>
@@ -14,10 +27,14 @@ export function GradeCurricularContent() {
           <GreadeDetails />
 
           <div className="flex gap-3">
-            <ClassStatus code={1} name="Concluída" status="concluida" />
-            <ClassStatus code={1} name="Pendente" status="pendente" />
-            <ClassStatus code={1} name="Não Concluída" status="naoConcluida" />
-            <ClassStatus code={1} name="Equivalente" />
+            {classStatuses.map((classStatus) => (
+              <ClassStatus
+                key={classStatus.name}
+                code={classStatus.code}
+                name={classStatus.name}
+                status={classStatus.status}
+              />
+            ))}
           </div>
         </div>
       </CardContent>
