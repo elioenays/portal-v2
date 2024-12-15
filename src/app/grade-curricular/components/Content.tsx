6b3d9e3f1fd04dcae1735024307a66a74f5fcdb1
Elioenay Silva <elionay407@gmail.com>
2024-12-15 12:11:23 -0300

    cria accordions da grade curricular

--- src/app/grade-curricular/components/Content.tsx
+++ src/app/grade-curricular/components/Content.tsx
@@ -1,6 +1,14 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { GreadeDetails } from './GreadeDetails'
 import { ClassStatus } from './ClassStatus'
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import { Separator } from '@/components/ui/separator'
 
 type ClassStatuses = {
   status?: 'concluida' | 'pendente' | 'naoConcluida' | 'default'
@@ -13,6 +21,27 @@ const classStatuses: ClassStatuses[] = [
   { code: 2, name: 'Pendente', status: 'pendente' },
   { code: 3, name: 'Não Concluída', status: 'naoConcluida' },
   { code: 4, name: 'Equivalente', status: 'default' },
+]
+
+const classCategories = [
+  { name: 'Todas', value: 'todas' },
+  { name: 'Disciplina Obrigatórias', value: 'disciplina-obrigatorias' },
+  {
+    name: 'Disciplinas Eletivas/Optativas',
+    value: 'disciplinas-eletivas-optativas',
+  },
+  {
+    name: 'Disciplinas Equivalentes',
+    value: 'disciplinas-equivalentes',
+  },
+  {
+    name: 'Componentes Curriculares',
+    value: 'componentes-curriculares',
+  },
+  {
+    name: 'Disciplinas Extras',
+    value: 'disciplinas-extras',
+  },
 ]
 
 export function GradeCurricularContent() {
@@ -36,6 +65,55 @@ export function GradeCurricularContent() {
               />
             ))}
           </div>
+
+          <ToggleGroup
+            type="single"
+            className="self-start"
+            defaultValue="todas"
+          >
+            <ToggleGroupItem value="todas">Todas</ToggleGroupItem>
+
+            <ToggleGroupItem value="em-curso">Em curso</ToggleGroupItem>
+
+            <ToggleGroupItem value="pendentes">Pendentes</ToggleGroupItem>
+
+            <ToggleGroupItem value="concluidos">Concluídos</ToggleGroupItem>
+          </ToggleGroup>
+
+          <Separator />
+
+          <ToggleGroup
+            type="single"
+            className="self-start"
+            defaultValue="todas"
+          >
+            {classCategories.map((classCategory) => (
+              <ToggleGroupItem
+                value={classCategory.value}
+                key={classCategory.name}
+              >
+                {classCategory.name}
+              </ToggleGroupItem>
+            ))}
+          </ToggleGroup>
+
+          <Accordion
+            type="single"
+            collapsible
+            className="w-full"
+            defaultValue="todas"
+          >
+            {classCategories.map((classCategory) => (
+              <AccordionItem
+                value={classCategory.value}
+                key={classCategory.name}
+              >
+                <AccordionTrigger>{classCategory.name}</AccordionTrigger>
+
+                <AccordionContent></AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
       </CardContent>
     </Card>
